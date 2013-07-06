ddd8b8216dad73a67dbc80c0120065e4e4dceb44
numnym <numnym@gmail.com>
2013-07-06 07:02:36 +0000

    Use index.html for home page

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var text = fs.readFileSynch('index.html', 'utf8');
+	response.send(text);
+	//response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
